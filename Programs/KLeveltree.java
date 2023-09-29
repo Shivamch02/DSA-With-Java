1146902d72c6539b2b77d7d4e7afb533818cce78
Shivam Chaurasiya <88436482+Shivamch02@users.noreply.github.com>
2023-09-29 08:57:55 +0530

    Add files via upload

---
+++ Programs/KLeveltree.java
@@ -0,0 +1,50 @@
+
+class KLeveltree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinayTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void KLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+        KLevel(root.left, level + 1, k);
+        KLevel(root.right, level + 1, k);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinayTree tree = new BinayTree();
+        Node root = tree.buildTree(nodes);
+        KLevel(root, 1, 3);
+    }
+}
