3bc96462ae4dd76d11c6516c97bf1dc2de51fcc8
Shivam Chaurasiya <88436482+Shivamch02@users.noreply.github.com>
2023-10-06 09:30:19 +0530

    Add files via upload

--- Programs/BinayTreesB.java
+++ Programs/BinayTreesB.java
@@ -294,6 +294,94 @@ public class BinayTreesB {
         return lca;
     }
 
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        if (rightLca == null) {
+            return leftLca;
+        }
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static int KAncestor(Node root, int n, int k) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+
+        return max + 1;
+
+    }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+
+        return data;
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinayTree tree = new BinayTree();
@@ -313,8 +401,13 @@ public class BinayTreesB {
         // topView(root);
         // int k = 2;
         // KLevel(root, 1, 0);
-        int n1 = 4;
-        int n2 = 6;
-        System.out.println(lca(root, n1, n2).data);
+        int n = 5;
+        // int n2 = 5;
+        int k = 2;
+        // System.out.println(lca2(root, n1, n2).data);
+        // System.out.println(minDist(root, n1, n2));
+        // KAncestor(root, n, k);
+        System.out.println(transform(root));
+        tree.levelOrder(root);
     }
 }
